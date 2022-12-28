7547b1c5205138ad6260229272c5a0f48cf7d64a
Mirek Rusin <mirek@me.com>
2022-12-28 12:28:36 +0100

    feat: adding ltrim

---
+++ src/ltrim.ts
@@ -0,0 +1,15 @@
+import map from './map.js'
+import sequence from './sequence.js'
+import type * as Parser from './parser.js'
+import ws0 from './ws0.js'
+
+/** @returns parser with left (default {@link ws0}) trim parser. */
+export function ltrim(
+  left: Parser.t<unknown> = ws0
+) {
+  return function <A>(a: Parser.t<A>): Parser.t<A> {
+    return map(sequence(left, a), _ => _[1])
+  }
+}
+
+export default ltrim
