308a80a5ef1676abab9187c51dde74b3c599a6d5
Mirek Rusin <mirek@me.com>
2023-10-24 20:36:09 +0200

    feat(regexp)!: improving regexp implementation

--- src/map-reason.test.ts
+++ src/map-reason.test.ts
@@ -1,6 +1,6 @@
 import * as P from './index.js'
 
 test('mapReason', () => {
-  const p = P.mapReason(P.re(/^\d/dy), _ => `<test>${_}</test>`)
-  expect(() => P.parse(p, 'a')).toThrow('<test>regex did not match</test>')
+  const p = P.mapReason(P.re(/\d/), _ => `<test>${_}</test>`)
+  expect(() => P.parse(p, 'a')).toThrow('<test>regexp /\\d/dy did not match</test>')
 })
