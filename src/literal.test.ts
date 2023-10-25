760a3e42eef33fc1db216feba94aacfe2441f4fc
Mirek Rusin <mirek@users.noreply.github.com>
2023-10-25 19:50:16 +0200

    refactor(literal)!: merge longestLiteral's functionality into literal, alias lit  (#5)

--- src/literal.test.ts
+++ src/literal.test.ts
@@ -1,9 +1,20 @@
 import * as P from './index.js'
 
-test('literal', () => {
+test('single', () => {
   expect(P.parser(P.literal('a'))('a')).toEqual('a')
   const p = P.parser(P.either(P.literal('a'), P.literal('b')))
   expect(p('a')).toEqual('a')
   expect(p('b')).toEqual('b')
   expect(() => p('c')).toThrow('Expected b.')
+})
+
+test('longest', () => {
+  expect(P.parser(P.star(P.literal(
+    'a',
+    'aa'
+  )))('aaaaa')).toEqual([
+    'aa',
+    'aa',
+    'a'
+  ])
 })
