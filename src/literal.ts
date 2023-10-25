32334ef1a08f93fff834512531fae8460ef861c1
Mirek Rusin <mirek@me.com>
2023-10-25 19:48:43 +0200

    refactor(literal)!: longestLiteral -> literal

--- src/literal.ts
+++ src/literal.ts
@@ -1,14 +1,24 @@
-import type * as Parser from './parser.js'
-import * as Result from './result.js'
+import * as RadixTrie from '@prelude/radix-trie'
 import * as Reader from './reader.js'
+import * as Result from './result.js'
+import type * as Parser from './parser.js'
 
-export function literal<T extends string>(expected: T): Parser.t<T> {
-  return function (reader) {
-    return Reader.startsWith(reader, expected) ?
-      Result.eat(reader, expected.length) as Result.Ok<T> :
-      Result.fail(reader, `Expected ${expected}.`)
+export const literal =
+  <T extends string>(...literals: T[]): Parser.t<T> => {
+    if (literals.length === 1) {
+      return reader =>
+        Reader.startsWith(reader, literals[0]) ?
+          Result.eat(reader, literals[0].length) as Result.Ok<T> :
+          Result.fail(reader, `Expected ${literals[0]}.`)
+    }
+    const trie = RadixTrie.of(literals)
+    return reader => {
+      const length = RadixTrie.longestPrefixLength(trie, reader.input, reader.offset)
+      return length > 0 ?
+        Result.eat(reader, length) as Result.Ok<T> :
+        Result.fail(reader, `Expected one of ${literals.length} literals.`)
+    }
   }
-}
 
 export {
   literal as lit
