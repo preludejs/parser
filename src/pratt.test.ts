635e6bb3da2f92ac8661012839a7230563733e99
Mirek Rusin <mirek@me.com>
2024-02-04 13:07:15 +0100

    updating tests

--- src/pratt.test.ts
+++ src/pratt.test.ts
@@ -1,6 +1,23 @@
 import * as P from './index.js'
 
-test('pratt', () => {
+const pp =
+  (node: any): string => {
+    switch (node?.type) {
+      case 'prefix':
+        return `(${node.op} ${pp(node.rhs)})`
+      case 'infix':
+        return `(${node.op} ${pp(node.lhs)} ${pp(node.rhs)})`
+      case 'postfix':
+        return `(${node.op} ${pp(node.lhs)})`
+      default:
+        if (typeof node === 'string') {
+          return `${node}`
+        }
+        throw new Error(`Unexpected node: ${node}`)
+    }
+  }
+
+test('https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html', () => {
   const p = P.parser(P.pratt({
     prefix: {
       '-': [NaN, 9],
@@ -19,27 +36,20 @@ test('pratt', () => {
       '!': [11, NaN],
       '[': [11, NaN]
     }
-  }, P.Json.number))
-  expect(p('1+2*3')).toEqual(({
-    type: 'infix',
-    op: '+',
-    lhs: 1,
-    rhs: {
-      type: 'infix',
-      op: '*',
-      lhs: 2,
-      rhs: 3
-    }
-  }))
-  expect(p('1*2+3')).toEqual(({
-    type: 'infix',
-    op: '+',
-    lhs: {
-      type: 'infix',
-      op: '*',
-      lhs: 1,
-      rhs: 2
-    },
-    rhs: 3
-  }))
+  }, P.re(/\w+/i)))
+  const t = (s: string) => pp(p(s))
+  expect(t('1')).toEqual('1')
+  expect(t('1+2*3')).toEqual('(+ 1 (* 2 3))')
+  expect(t('1*2+3')).toEqual('(+ (* 1 2) 3)')
+  expect(t('a+b*c*d+e')).toEqual('(+ (+ a (* (* b c) d)) e)')
+  expect(t('f.g.h')).toEqual('(. f (. g h))')
+  expect(t('1+2+f.g.h*3*4')).toEqual('(+ (+ 1 2) (* (* (. f (. g h)) 3) 4))')
+  expect(t('--1*2')).toEqual('(* (- (- 1)) 2)')
+  expect(t('--f.g')).toEqual('(- (- (. f g)))')
+  // expect(t('-9!')).toEqual('(- (! 9))')
+  // expect(t('f.g!')).toEqual('(! (. f g))')
+  // expect('(((0)))').toEqual('0')
+  // expect('x[0][1]').toEqual('([ ([ x 0) 1)')
+  // expect('a?b:c?d:e').toEqual('(? a b (? c d e))')
+  // expect(t('a = 0 ? b : c = d')).toEqual('(= a (= (? 0 b c) d))')
 })
