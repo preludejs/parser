80a4d38e022db4a545508e214d74888bd0599f0a
Mirek Rusin <mirek@me.com>
2024-03-24 14:21:53 +0100

    feat: liftable right

---
+++ src/right.test.ts
@@ -0,0 +1,6 @@
+import * as P from './index.js'
+
+test('right', () => {
+  const p = P.parser(P.right('=', P.re(/\d+/)))
+  expect(p('=123')).toEqual('123')
+})
--- src/right.ts
+++ src/right.ts
@@ -4,7 +4,7 @@ import type * as Parser from './parser.js'
 
 /** @returns `b` after successful `a` and `b` sequence match. */
 export const right =
-  <B>(a: Parser.Liftable, b: Parser.t<B>): Parser.t<B> =>
+  <T extends Parser.Liftable>(a: Parser.Liftable, b: T): Parser.t<Parser.Parsed<T>> =>
     map(pair(a, b), _ => _[1])
 
 export default right
