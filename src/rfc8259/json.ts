ffa0cfca5e94a7afd3f9a35b1e772d0b092f5cf1
Mirek Rusin <mirek@me.com>
2024-03-24 14:23:03 +0100

    chore: better typing in json

--- src/rfc8259/json.ts
+++ src/rfc8259/json.ts
@@ -1,15 +1,17 @@
 import array from './array.js'
 import false_ from './false.js'
+import first from '../first.js'
 import null_ from './null.js'
 import number_ from './number.js'
 import object_ from './object.js'
 import string_ from './string.js'
 import trim from '../trim.js'
 import true_ from './true.js'
-import first from '../first.js'
+import type * as Reader from '../reader.js'
+import type * as Parser from '../parser.js'
 
-export const value =
-  reader =>
+export const value: Parser.t<unknown> =
+  (reader: Reader.t) =>
     first(
       string_,
       number_,
