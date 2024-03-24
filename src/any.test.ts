68d2ffc3298c9b2f14c4611b92d16c9f4714523c
Mirek Rusin <mirek@users.noreply.github.com>
2024-03-24 13:59:31 +0100

    feat: adding any (#10)

---
+++ src/any.test.ts
@@ -0,0 +1,10 @@
+import * as P from './index.js'
+
+test('any', () => {
+  const p = P.parser(
+    P.map(P.seq('[', P.any, ']'), _ => _[1])
+  )
+  expect(p('[a]')).toEqual('a')
+  expect(() => p('[]')).toThrow('Failed sequence. Expected ].')
+  expect(() => p('[  ]')).toThrow('Failed sequence. Expected ].')
+})
