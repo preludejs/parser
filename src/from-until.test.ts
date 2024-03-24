5189ce8353471bf53f65d8ebea2cd34fbd36f545
Mirek Rusin <mirek@me.com>
2024-03-24 14:46:53 +0100

    feat: adding from-until

---
+++ src/from-until.test.ts
@@ -0,0 +1,7 @@
+import * as P from './index.js'
+
+test('from-until', () => {
+  const p = P.parser(P.fromUntil('BEGIN', 'END'))
+  expect(p('BEGIN END')).toEqual({ head: 'BEGIN', inner: ' ', tail: 'END' })
+  expect(p('BEGIN foo END')).toEqual({ head: 'BEGIN', inner: ' foo ', tail: 'END' })
+})
