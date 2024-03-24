4515cbfc646a7295ac49e777a311d2d86be1e77f
Mirek Rusin <mirek@users.noreply.github.com>
2024-03-24 14:46:18 +0100

    test: run tsc (#11)

--- src/regexp.ts
+++ src/regexp.ts
@@ -12,7 +12,7 @@ type Indices = [number, number][] & { groups?: Record<string, [number, number]> 
 export function regexp(
   regexp: RegExp,
   valueGroup: number | string = 0,
-  advanceGroup: number | string = 0,
+  advanceGroup: number | string = 0
 ): Parser.t<string> {
   const re = new RegExp(regexp.source, 'dy' + regexp.flags.replace(/[dyg]/g, ''))
   return function (reader) {
@@ -22,8 +22,8 @@ export function regexp(
       return Result.fail(reader, `regexp ${re} did not match`)
     }
     const valueString =
-      typeof valueGroup === 'string' && match.groups ?
-        match.groups[valueGroup] :
+      typeof valueGroup === 'string' ?
+        match.groups?.[valueGroup] :
         match[valueGroup]
     if (typeof valueString !== 'string') {
       throw new Error('invalid value group')
