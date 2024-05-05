8722280c179e7b5e6aeed2de5ab2fbb442862b62
Mirek Rusin <mirek@me.com>
2024-05-05 15:15:24 +0200

    chore: drop console.log from test

--- src/line.test.ts
+++ src/line.test.ts
@@ -2,7 +2,6 @@ import * as P from './index.js'
 
 test('line', () => {
   const parser = P.parser(P.star(P.line))
-  console.log(parser('hello\nworld\r\n'))
   expect(parser('hello\nworld\n')).toEqual([
     'hello',
     'world'
