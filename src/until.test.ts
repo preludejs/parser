48e953fc9f4ef70bfcd2a692ad9305bb37fe9237
Mirek Rusin <mirek@users.noreply.github.com>
2024-08-27 07:13:34 +0200

    feat!: error context (#14)

--- src/until.test.ts
+++ src/until.test.ts
@@ -5,5 +5,8 @@ test('until', () => {
   const p = P.parser(P.until('END'))
   expect(p('END')).toEqual({ head: '', tail: 'END' })
   expect(p('foo END')).toEqual({ head: 'foo ', tail: 'END' })
-  expect(() => p('foo END bar')).toThrow('Expected exhaustive result, unparsed 4.')
+  expect(() => p('foo END bar')).toThrow(`Expected exhaustive result, parsed 7 (unparsed 4).
+
+foo END bar
+       ^ 1:8`)
 })
